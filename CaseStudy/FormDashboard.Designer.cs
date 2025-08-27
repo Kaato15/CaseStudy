7dcb485d1518aa235af6774298b405f490fb8478
Kurt Louie Castro <castrokeilo15@gmail.com>
2025-08-27 23:37:27 +0800

    change color dashboard

--- CaseStudy/FormDashboard.Designer.cs
+++ CaseStudy/FormDashboard.Designer.cs
@@ -35,6 +35,7 @@ namespace CaseStudy
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(31)))), ((int)(((byte)(68)))));
             this.ClientSize = new System.Drawing.Size(1114, 601);
             this.Name = "FormDashboard";
             this.Text = "Dashboard";
